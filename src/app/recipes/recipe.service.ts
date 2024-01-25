37e6de550e3154fcb48305c9ce40ea86009f2ecf
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2024-01-25 11:30:04 +0200

    Added temp ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,28 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService{
 
   recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'I am just testing','../../assets/imgs/IMG-20240110-WA0022.jpg'),
-        new Recipe('A new test recipe', 'I am just testing','../../assets/imgs/IMG-20240110-WA0022.jpg'),
+        new Recipe(
+          'A test recipe', 
+          'I am just testing',
+          '../../assets/imgs/IMG-20240110-WA0022.jpg',
+          [
+            new Ingredient('Test 1', 3),
+            new Ingredient('Test 2', 30)
+          ]),
+        new Recipe(
+          'A new test recipe', 
+          'I am just testing',
+          '../../assets/imgs/IMG-20240110-WA0022.jpg',
+          [
+            new Ingredient('Test 3', 1),
+            new Ingredient('Test 4', 5)
+          ]),
       ];
 
       getRecipes(){
